7e36cbc913d3deb8fc4f4eeb010938b758a64362
Abelardo Vieira Mota <abevieiramota@gmail.com>
2015-06-27 23:57:04 -0300

    refatorado IsCamelCaseRule para usar apenas uma re

--- src/br/ufc/rulesengine/core/IsCamelCaseRule.java
+++ src/br/ufc/rulesengine/core/IsCamelCaseRule.java
@@ -4,31 +4,12 @@ import java.util.regex.Pattern;
 
 public class IsCamelCaseRule {
 
-	private static final Pattern _blank_char = Pattern.compile("\\s");
 	private static final Pattern _single_camel_case_piece = Pattern
-			.compile("^[A-Z][a-z]*$");
+			.compile("^([A-Z][a-z]*)(\\s[A-Z][a-z]*)*$");
 
 	public boolean apply(String nome) {
 
-		final String[] nomeSplitted = _blank_char.split(nome);
-
-		boolean isInCamelCase = true;
-
-		for (String piece : nomeSplitted) {
-
-			// if (!Character.isUpperCase(piece.charAt(0))) {
-			//
-			// isInCamelCase = false;
-			// }
-
-			System.out.println(piece);
-			if (!_single_camel_case_piece.matcher(piece).find()) {
-
-				isInCamelCase = false;
-			}
-		}
-
-		return isInCamelCase;
+		return _single_camel_case_piece.matcher(nome).find();
 	}
 
 }
